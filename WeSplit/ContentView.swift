1034991be14af5ef1434cbcad4fea18e3a8d54b0
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-15 12:59:29 -0600

    Learning about Forms Groups

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            /* Swift only allows to use 10 childs
+             inside of views
+             for that reason we have to use
+             Groups or Sections if we want more */
+            Group {
+                Text("Hello World")
+                Text("Hello, Mom")
+                Text("Hello, Girl")
+                Text("Hello, Mom")
+            }
+            Group {
+                Text("Hello, Mario")
+                Text("Hello, Link")
+                Text("Hello, Captain Price")
+                Text("Hello, Messi")
+                Text("Hello, Dk")
+
+            }
         }
-        .padding()
     }
 }
 
