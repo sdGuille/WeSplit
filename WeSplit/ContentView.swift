b40846bb5e442d7d840212cb4ecdab3e121b6c18
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-15 13:34:53 -0600

    Learning about Bindings
    Notice that in the second Text we just using \(name) because we only need read the value

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
+        Form {
+            TextField("Write your name here", text: $name) // $ biding
+            Text("Welcome \(name), We are glad get you here")
         }
     }
 }
