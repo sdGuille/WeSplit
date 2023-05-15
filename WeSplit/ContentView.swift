127336e7440142dcbdd6bbdf42f28e18314fee12
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-15 13:10:19 -0600

    NavigationView and navigation title

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,26 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Form {
-            /* Swift only allows to use 10 childs
-             inside of views
-             for that reason we have to use
-             Groups or Sections if we want more */
-            Group {
-                Text("Hello World")
-                Text("Hello, Mom")
-                Text("Hello, Girl")
-                Text("Hello, Mom")
+        NavigationView {
+            Form {
+                /* Swift only allows to use 10 childs
+                 inside of views
+                 for that reason we have to use
+                 Groups or Sections if we want more */
+                Section {
+                    Text("Hello World")
+                    Text("Hello, Mom")
+                    Text("Hello, Girl")
+                    Text("Hello, Mom")
+                }
             }
-            Group {
-                Text("Hello, Mario")
-                Text("Hello, Link")
-                Text("Hello, Captain Price")
-                Text("Hello, Messi")
-                Text("Hello, Dk")
-
-            }
+            .navigationTitle("SwiftUI")
         }
     }
 }
