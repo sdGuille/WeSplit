72c1287027441f1c5831b49623ab1d8d4964fb94
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-15 10:33:28 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by MacBook Pro on 15/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
